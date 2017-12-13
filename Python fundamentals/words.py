9d85e30b8c1e5fcafeae22423923510980636f1f
Ankit Kumar <ankit.kumar02@outlook.com>
2017-12-13 23:04:55 +0530

    Add documentation comments for module level and functions

--- Python fundamentals/words.py
+++ Python fundamentals/words.py
@@ -1,9 +1,23 @@
+"""Retrieve and prints words from a URL.
+
+Usage :
+
+    python3 words.py <URL>
+"""
 import sys
 from urllib.request import urlopen
 
 
 #  url == http://sixty-north.com/c/t.txt
 def fetch_words(url):
+    """Fetch a list of words from the URL.
+    
+    Args:
+        url : The URL of a UTF-8 text document
+    
+    Returns:
+        A list of strings containing the words from the document.  
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -13,16 +27,28 @@ def fetch_words(url):
 
     return story_words
 
+
 def print_items(items):
+    """Prins one item per line.
+    
+    Args:
+        items : An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
+
 def main(url):
+    """Prints each word from a text document from a URL
+    
+    Args:
+        url : The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
 
 # sys.argv[1]  represents the command line argument like while executing the module below:
-# python words.py http://sixty-north.com/c/t.txt     
+# python words.py        
 if(__name__=="__main__"):
-    main(sys.argv[1])
+    main(sys.argv[1])  #The 0th argument of sys.argv is the module filename. So, we use 1st argument.
