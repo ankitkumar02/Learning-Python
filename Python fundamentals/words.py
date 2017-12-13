c56ee41bcb2dc2599a04d270929d8c9d491603b6
Ankit Kumar <ankit.kumar02@outlook.com>
2017-12-13 22:37:04 +0530

    Added code for command line arguments

--- Python fundamentals/words.py
+++ Python fundamentals/words.py
@@ -1,14 +1,28 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+
+#  url == http://sixty-north.com/c/t.txt
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
 
-    for word in story_words:
-        print(word)    
+    return story_words
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
+
+
+# sys.argv[1]  represents the command line argument like while executing the module below:
+# python words.py http://sixty-north.com/c/t.txt     
 if(__name__=="__main__"):
-    fetch_words()
+    main(sys.argv[1])
