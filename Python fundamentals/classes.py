276555a7b5d37b3d15280b576e7d50a889acdb67
Ankit Kumar <ankit.kumar02@outlook.com>
2017-12-24 12:02:08 +0530

    Implementin inheritance

--- Python fundamentals/classes.py
+++ Python fundamentals/classes.py
@@ -121,25 +121,26 @@ class Flight:
 #     def seating_plan(self):
 #         return (range(1, self._num_rows + 1), "ABCDEFGHJK"[:self._num_seats_per_row])
 
-class AirbusA319:
+class Aircraft:
     def __init__(self, registration):
         self._registration = registration
 
     def registration(self):
         return self._registration
 
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len (row_seats)
+
+class AirbusA319(Aircraft):
+   
     def model(self):
         return "Airbus A319"
 
     def seating_plan(self):
         return range(1, 23), "ABCDEF"
 
-class Boeing777:
-    def __init__(self, registration):
-        self._registration = registration
-
-    def registration(self):
-        return self._registration
+class Boeing777(Aircraft):
 
     def model(self):
         return "Boeing 777"
